a366ec39ccffa67fc4300d6011a35622abbe09b4
Kitton <alval.bel@gmail.com>
2014-01-17 15:06:44 +0100

    +Some improvments to Case class

--- src/CBR/Case.java
+++ src/CBR/Case.java
@@ -7,10 +7,21 @@ public class Case {
 	private int classLabel;
 	private boolean isCorrect;
 	
+	public Case(List<Integer> attributes) {
+		this.attributes = attributes;
+	}
+	
 	public Case(List<Integer> attributes, int classLabel, boolean isCorrect) {
 		this.attributes = attributes;
 		this.classLabel = classLabel;
 		this.isCorrect = isCorrect;
+	}
+	
+	public void setClassLabel(int value){
+		this.classLabel = value;
+	}
+	public void setIscorrect(boolean value){
+		this.isCorrect = value;
 	}
 	
 	public List<Integer> getAttributes() { return attributes;}
