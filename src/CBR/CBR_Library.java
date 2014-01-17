f354609212090b876ff4057a8dc36a6da229f561
Kitton <alval.bel@gmail.com>
2014-01-17 14:48:45 +0100

    *Implement library as list

--- src/CBR/CBR_Library.java
+++ src/CBR/CBR_Library.java
@@ -4,19 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CBR_Library {
+	private static List<Case> cases;
+	
 	public static void createTree(List<Case> cases){
-		
+		CBR_Library.cases = cases;
 	}
 	
 	public static List<Case> retriveClosestCases(Case inputCase){
-		return new ArrayList<Case>();
+		return CBR_Library.cases;
 	}
 	
 	public static void addCase(Case inputCase){
-		
+		cases.add(inputCase);
 	}
 	
 	public static void deleteCase(Case inputCase){
-		
+		cases.remove(inputCase);
 	}
 }
