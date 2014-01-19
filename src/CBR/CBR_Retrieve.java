f93e1a53c13c2d89b64c05eb218a9e4fa127a535
sourta24 <christiana_chalka@hotmail.com>
2014-01-19 20:25:37 +0100

    Update CBR_Retrieve.java

--- src/CBR/CBR_Retrieve.java
+++ src/CBR/CBR_Retrieve.java
@@ -8,6 +8,8 @@ public class CBR_Retrieve {
 	
 	private Case NewCase;	
 	private int K;
+	private String DistanceMeasure;
+	private double [] AttributeWeights;
 	
 	
 	
@@ -18,13 +20,27 @@ public class CBR_Retrieve {
 				
 	}
 	
+	public CBR_Retrieve(int k, String type)
+	{
+		this.K = k;
+		this.DistanceMeasure = type;
+		
+	}
+	
+	public CBR_Retrieve(int k, String type, double []weights)
+	{
+		this.K = k;
+		this.DistanceMeasure = type;
+		this.AttributeWeights = weights;
+	}
+	
 	//get K Nearest Neighbors
 	public List<Case> getkNN(List<Case> PastCases,Case TestCase,int k)
 	{
 		List<Case> kNearestNeighbors;
 		kNN algorithm = new kNN(PastCases,K);		
 		
-		kNearestNeighbors = algorithm.getNearestNeighbors(TestCase);
+		kNearestNeighbors = algorithm.getNearestNeighbors(TestCase,DistanceMeasure,AttributeWeights);
 		
 		return kNearestNeighbors;
 	}
