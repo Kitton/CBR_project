b4f28c97ed3bc79a4ef005b5f715b0fc0f56988f
Oscar Amoros Huguet <oscaramoroshuguet@Macbook-Air.local>
2014-01-19 12:18:15 +0100

    Added changes to Library

--- src/CBR/CBR_Library.java
+++ src/CBR/CBR_Library.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class CBR_Library {
 	private static List<Case> cases;
-	int size;
 	
 	public static void createTree(List<Case> cases){
 		CBR_Library.cases = cases;
@@ -15,25 +14,13 @@ public class CBR_Library {
 		return CBR_Library.cases;
 	}
 	
-	/*public static void addCase(Case inputCase){
+	public static void addCase(Case inputCase){
 		cases.add(inputCase);
-	}*/
+	}
 	
-	/*public static void deleteCase(Case inputCase){
+	public static void deleteCase(Case inputCase){
 		cases.remove(inputCase);
-	}*/
-	
-	// Add a new case in the CBR Library
-    public  void addCase(Case inputCase){
-    	cases.add(inputCase);
-    	this.size ++; 
-    }
-    
-    // Delete a case in the CBR Library
-    public  void deleteCase(Case inputCase){
-    	cases.remove(inputCase);
-    	this.size --;
-    }
+	}
     
     // Return the cases that are in the CBR Library
     public List<Case> getLibraryCases()
@@ -44,6 +31,6 @@ public class CBR_Library {
     //Return the number of cases stored in the CBR Library
     public int getNumberCases()
     {
-    	return this.size;
+    	return this.cases.size();
     }
 }
