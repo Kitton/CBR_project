aca09d40f64f88122763eb91a3cbfc38232df1d3
sourta24 <christiana_chalka@hotmail.com>
2014-01-19 15:00:13 +0100

    Update CBR_Main.java

--- src/CBR/CBR_Main.java
+++ src/CBR/CBR_Main.java
@@ -9,11 +9,51 @@ import java.util.List;
 
 public class CBR_Main {
 	public static void main(String[] args) throws IOException {
-		List<Case> trainingData = DataReader.readData("./data/pendigits.tra");
-		System.out.println(trainingData.size() + " training cases were read");
-		CBR_Library library = new CBR_Library(trainingData);
-		
-		List<Case> testData = DataReader.readData("./data/pendigits.tes");
-		System.out.println(testData.size() + " test cases were read");
+		List<Case> trainingData = DataReader.readData("./data/pendigits.tra");		
+        	System.out.println(trainingData.size() + " cases were read ");
+        
+        	//Store Cases to the CBR Library
+        	CBR_Library library = new CBR_Library(trainingData);
+        
+       
+	 	List<Case> testingData = DataReader.readData("./data/pendigits.tes");
+        	int TestDataSize = testingData.size();
+        	System.out.println(TestDataSize + " cases were read");
+        
+        	int k =3;
+        	CBR_Retrieve retrieval = new CBR_Retrieve(k);
+        	CBR_Reuse reuse = new CBR_Reuse();
+       
+        
+        	for (int i=0; i<TestDataSize; i++)
+        	{
+        	
+        		Case NewCase = testingData.get(i);
+        		List<Case> ClosestCases;
+        		List<Case> NearestNeighbors;
+        		int classPredicted;
+        	
+        		System.out.println("######################################");
+        		System.out.println(NewCase);
+        	
+        		//Retrieve Phase
+        		ClosestCases = library.retriveClosestCases(NewCase);
+        	
+        		NearestNeighbors = retrieval.getkNN(ClosestCases, NewCase, k);
+        	
+        		//Reuse Phase
+        		classPredicted = reuse.getNewCaseClassLabel(NearestNeighbors, NewCase);
+        	
+        		NewCase.setPredictedClassLabel(classPredicted);
+        	
+        		//Evaluate-Retain
+        	
+        	
+        	
+        	}
+        	
+        	
+        	
+        	
 	}
 }
